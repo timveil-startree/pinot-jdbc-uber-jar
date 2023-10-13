829f8ebb3296d33d4995bb8f476f3daf31d9a013
Tim Veil <3260845+timveil@users.noreply.github.com>
2023-10-13 15:52:40 -0400

    trying to get this working

---
+++ src/main/java/Harness.java
@@ -0,0 +1,11 @@
+import org.apache.pinot.common.version.PinotVersion;
+
+public class Harness {
+
+    public static void main(String[] args) {
+
+        System.out.println(PinotVersion.VERSION);
+        System.out.println(PinotVersion.VERSION_METRIC_NAME);
+
+    }
+}
