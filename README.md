f8f215be6f20c37c5e69894d70f10cf523d28a0c
Tim Veil <3260845+timveil@users.noreply.github.com>
2023-10-16 15:48:03 -0400

    cleaning up

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # pinot-jdbc-uber-jar
+
+as property - useMultistageEngine=true
+
+in url - brokers=localhost:8099
+
+"Do not escape JDBC metadata calls" must be selected
--- src/main/java/Harness.java
+++ src/main/java/Harness.java
@@ -1,11 +1,10 @@
 import org.apache.pinot.common.version.PinotVersion;
 
+
 public class Harness {
 
     public static void main(String[] args) {
-
         System.out.println(PinotVersion.VERSION);
         System.out.println(PinotVersion.VERSION_METRIC_NAME);
-
     }
 }
